bf7783b0dee5567c60e6e49e256f7f40843f73f2
PatTMooK <padthadon55555@gmail.com>
2025-04-01 19:14:46 +0700

    Update 7_Website_DemoQA_CheckBox.spec.ts.spec.ts

--- tests/7_Website_DemoQA_CheckBox.spec.ts.spec.ts
+++ tests/7_Website_DemoQA_CheckBox.spec.ts.spec.ts
@@ -2,16 +2,20 @@ import { test, expect } from '@playwright/test';
 
 test.describe('Website QADEMO Checkbox', () => {
     // npx playwright test "7_Website_DemoQA_CheckBox.spec.ts.spec.ts" 
-    // โจทย์ 1: ตรวจสอบการเลือก checkbox จุดประสงค์: ตรวจสอบว่า checkbox ถูกเลือกแล้วชื่อไฟล์/โฟลเดอร์แสดงในข้อความด้านล่าง คลิกเช็ค Desktop > Notes, Commands ตรวจสอบว่า text ด้านล่างมีคำว่า "You have selected: desktop notes commands"
+    // โจทย์ 1: ตรวจสอบการเลือก checkbox จุดประสงค์: ตรวจสอบว่า checkbox ถูกเลือกแล้วชื่อไฟล์/โฟลเดอร์แสดงในข้อความด้านล่าง คลิกเช็ค Desktop > Notes ตรวจสอบว่า text ด้านล่างมีคำว่า "You have selected: desktop notes"
 
     test('Checkbox', async ({ page }) => {
 
         await page.goto("https://demoqa.com/checkbox", { waitUntil: 'domcontentloaded' });
+        // ไปยังเว็บทดสอบการ checkbox แบบไม่รอโหลดนานเกินไป
         await page.locator("//div[@id='tree-node']/ol/li/span/button[@type='button']").click();
+        // ทำการกดปุ่มที่ต้องการเพื่อดูไฟล์ด้านใน
         await page.locator("//label[@for='tree-node-desktop']/preceding-sibling::button[@type='button']").click();
-        await page.pause();
+        // ทำการกดปุ่มที่ต้องการเพื่อดูไฟล์ด้านใน  node desktop
         await page.locator("//label[@for='tree-node-notes']/span[@class='rct-checkbox']").click();
-        await expect.soft(page.locator("//div[@id='result']")).toContainText("You have selected :")
+        // ทำการกดปุ่ม checkbox เป็นคำว่า note
+        await expect.soft(page.locator("//div[@id='result']")).toContainText("You have selected :notes")
+        // ตรวจสอบข้อความการเลือก ที่แถบ Result ต้องมีคำที่กไหนด
         
         
     })
