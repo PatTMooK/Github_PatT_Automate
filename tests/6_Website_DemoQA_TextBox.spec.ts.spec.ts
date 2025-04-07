80a2a99ddc424a506c701eff27229265e7c822c5
PatTMooK <padthadon55555@gmail.com>
2025-04-08 02:25:45 +0700

    Update 6_Website_DemoQA_TextBox.spec.ts.spec.ts

--- tests/6_Website_DemoQA_TextBox.spec.ts.spec.ts
+++ tests/6_Website_DemoQA_TextBox.spec.ts.spec.ts
@@ -1,22 +1,48 @@
 import { test, expect } from '@playwright/test';
+import { count } from 'console';
 
 test.describe('Website QADEMO Textbox', () => {
     // npx playwright test "6_Website_DemoQA_TextBox.spec.ts.spec.ts"
 
-    test('Textbox', async ({ page }) => {
+    test('TC01_Fill&Submit', async ({ page }) => {
+        // npx playwright test "6_Website_DemoQA_TextBox.spec.ts.spec.ts" -g "TC01_Fill&Submit$"  
 
-        await page.goto("https://demoqa.com/", { waitUntil: 'domcontentloaded' });
-        await page.locator('//div [@class="card-body"]/h5[contains (text(),"Elements")]').click();
-        await page.locator('//ul[@class="menu-list"]//li[@id="item-0"]//span[text()="Text Box"]').click();
-        await page.locator('//input [@placeholder="Full Name"]').fill("Padthadon Kimlaung");
-        await page.locator('//input [@placeholder="name@example.com"]').fill("Padthadon@mail.com");
-        await page.locator('//textarea[@placeholder="Current Address"]').fill("Bang Sue,Bangkok");
-        await page.locator('//textarea[@id="permanentAddress"]').fill("Huai Krachao,Kanchanaburi");
-        await page.locator('//button [@id="submit"]').click();
+        await page.goto("https://demoqa.com/text-box", { waitUntil: 'domcontentloaded' }); 
+        // เปิดหน้าเว็บ
+        
+        const Name = ['Padthadon Kimlaung']; 
+        const Email = ['Padthadon@gmail.com'];
+        const currentAddress = 'Bang Sue,Bangkok,10800,Thailand';
+        const permanentAddress = 'Huai Krachao,Kanchanaburi,71170,Thailand';
+        //ข้อมูล input
+        const expectedResults = [
+            `Name:${Name}`,
+            `Email:${Email}`,
+            `Current Address :${currentAddress}`,
+            `Permananet Address :${permanentAddress}`
+            ];
+        // ข้อมูลที่คาดหวังหลัง Submit
 
-        const BoxAfterSubmit = await page.locator('//div [@class="border col-md-12 col-sm-12"]').isVisible();
-        expect(BoxAfterSubmit).toBeTruthy();
-        // await page.pause();
+        await page.locator("//input[@placeholder='Full Name']").fill(`${Name}`);
+        await page.locator("//input[@placeholder='name@example.com']").fill(`${Email}`);
+        await page.locator("//textarea[@placeholder='Current Address']").fill(`${currentAddress}`);
+        await page.locator("//textarea[@id='permanentAddress']").fill(`${permanentAddress}`);
+        // กรอกข้อมูลในฟอร์ม
+        await page.locator("//button[@id='submit']").click();
+        // กดปุ่ม Submit
+        
+        const outputBox = page.locator("//div[@id='output']");
+        // กำหนดให้ outputBox เก็บค่า Element output
+        await expect.soft(outputBox).toBeVisible();
+        // ครวจสอบว่า Element output แสดงขึ้นมา
+
+        for (const expectedText of expectedResults) {
+            await expect.soft(outputBox).toContainText(expectedText);
+        }
+        // ตรวจสอบผลลัพธ์แต่ละบรรทัด
+        // await console.log(expectedResults);
+        
+        
         
     })
     
