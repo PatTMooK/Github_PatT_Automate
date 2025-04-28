b4a8cf45454cf1a3bc58d12ace0fc29c543d5e9f
PatTMooK <padthadon55555@gmail.com>
2025-04-28 21:06:56 +0700

    Update 8.2_Reg_Login&logout.spec.ts

--- tests/8.2_Reg_Login&logout.spec.ts
+++ tests/8.2_Reg_Login&logout.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 test.describe('Regression_Login_Logout', () => {
-    // npx playwright test "2_Reg_Login&logout.spec.ts"
+    // npx playwright test "9_Reg_Login&logout.spec.ts"
     test('01_Not_Add_user&password', async ({ page }) => {
         //  npx playwright test "2_Reg_Login&logout.spec.ts" -g "01_Not_Add_user&password"
         await page.goto('https://practice.expandtesting.com/login', { waitUntil: 'domcontentloaded' });
